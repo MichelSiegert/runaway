676b318d791da517013d22c5127e09a0b252e09b
Michel Siegert <michel.siegert@student.fh-kiel.de>
2021-10-09 17:03:37 +0200

    added a ListView that is Scrollable, as a prove of concept, that is going to be as large as the entire side.

--- lib/locations_scaffold.dart
+++ lib/locations_scaffold.dart
@@ -8,9 +8,28 @@ Scaffold locationScaffold( final String title, BuildContext context) => Scaffold
     backgroundColor: Colors.green,
   ),
   //Text in the middle of the page
-  body: const Center(
-    child: Text('Locations hinzufügen'),
+  body: Center(
+    child:SizedBox.expand(
+      child: ListView(
+        children: populateListView(context)
+      ),
+    )
   ),
   //Menu Sidebar
   drawer: createSidebar(context),
+);
+
+
+List<ListTile> populateListView(BuildContext context) {
+  List<ListTile> elemente = [];
+  List<int> liste= [];
+  [for (int i=0; i<50; i++) liste.add(i)];
+  for (int zahl in liste) {
+    elemente.add(createTile(zahl, context));
+  }
+  return elemente;
+}
+
+ListTile createTile(final int inhalt, BuildContext context) => ListTile(
+  title: Text(inhalt.toString()),
 );
