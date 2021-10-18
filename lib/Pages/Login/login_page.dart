1c423e468f19f107dd172ccdcc4068da1e28d0c9
Michel Siegert <michel.siegert@student.fh-kiel.de>
2021-10-18 10:34:07 +0200

    adding a login page.

---
+++ lib/Pages/Login/login_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import 'login_scaffold.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return loginScaffold(title,context);
+  }
+}
