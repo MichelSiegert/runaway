6e44a032e2087c9c4754a83bb1199aba4b47181f
fatima.s.benkhelifa <fatima.s.benkhelifa@student.fh-kiel.de>
2021-11-26 18:52:41 +0100

    Location at first page after Login

--- lib/Pages/Menu/menu_scaffold.dart
+++ lib/Pages/Menu/menu_scaffold.dart
@@ -1,16 +1,77 @@
 import '../../Widgets/sidebar.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:geocoding/geocoding.dart';
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+class _HomePageState extends State<HomePage> {
+  Position? _currentPosition;
+  String? _currentAddress;
+
+  @override
+  Widget build(BuildContext context) {
+    return menuScaffold("Runaway", context);
+  }
 
 //
-Scaffold menuScaffold(final String title, BuildContext context) => Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-        backgroundColor: Colors.green,
-      ),
-      //Text in the middle of the page
-      body: const Center(
-        child: Text('Regenradar hinzufügen'),
-      ),
-      //Menu Sidebar
-      drawer: createSidebar(context),
-    );
+  Scaffold menuScaffold(final String title, BuildContext context) =>
+      Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+          backgroundColor: Colors.green,
+        ),
+        //Text in the middle of the page
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              if (_currentAddress != null) Text(
+                  _currentAddress!
+              ),
+              TextButton(
+                child: Text("Get location"),
+                onPressed: () {
+                  _getCurrentLocation();
+                },
+              )
+            ],
+          ),
+        ),
+        //Menu Sidebar
+        drawer: createSidebar(context),
+      );
+
+  _getCurrentLocation() {
+    Geolocator
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.best,
+        forceAndroidLocationManager: true)
+        .then((Position position) {
+      setState(() {
+        _currentPosition = position;
+        _getAddressFromLatLng();
+      });
+    }).catchError((e) {
+      print(e);
+    });
+  }
+
+  _getAddressFromLatLng() async {
+    try {
+      List<Placemark> placemarks = await placemarkFromCoordinates(
+          _currentPosition!.latitude,
+          _currentPosition!.longitude
+      );
+
+      Placemark place = placemarks[0];
+
+      setState(() {
+        _currentAddress =
+        "${place.locality}, ${place.postalCode}, ${place.country}";
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+}
