72614f2050a3b738bbb9533ff09433c7f82c9b6e
MichelSiegert <40004350+MichelSiegert@users.noreply.github.com>
2021-12-09 19:36:52 +0100

    addign a fixed size to the dialog.+
    REMOVING GARBAGE CODE!

--- lib/Widgets/loading_animation.dart
+++ lib/Widgets/loading_animation.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
 
-Future loadingAnimation(BuildContext context) =>
-    showDialog(
+Future loadingAnimation(BuildContext context) => showDialog(
       context: context,
       builder: (BuildContext context) {
         return Dialog(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: const [CircularProgressIndicator(),
-              Text("loading")]
-          )
-        );
+            child:SizedBox(
+              //TODO make this not hard coded!
+            height:100,
+            child: Column(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: const [
+              CircularProgressIndicator(),
+              Text("loading")
+            ])));
       },
     );
-
