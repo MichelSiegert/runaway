8fc903c7092618c3d0e260506ab7038312a518be
MichelSiegert <40004350+MichelSiegert@users.noreply.github.com>
2021-12-09 19:40:20 +0100

    improving readability of the code!

--- lib/Widgets/loading_animation.dart
+++ lib/Widgets/loading_animation.dart
@@ -1,19 +1,16 @@
 import 'package:flutter/material.dart';
 
 Future loadingAnimation(BuildContext context) => showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return Dialog(
-            child:SizedBox(
-              //TODO make this not hard coded!
-            height:100,
+    context: context,
+    builder: (BuildContext context) => Dialog(
+        child: SizedBox(
+            //TODO make this not hard coded!
+            height: 100,
             child: Column(
                 mainAxisSize: MainAxisSize.max,
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: const [
-              CircularProgressIndicator(),
-              Text("loading")
-            ])));
-      },
-    );
+                  CircularProgressIndicator(),
+                  Text("loading")
+                ]))));
