8df6dcf0d65ad214547e0816de8f4a689123b602
Michel Siegert <michel.siegert@student.fh-kiel.de>
2021-12-11 12:33:42 +0100

    adding a permissions to manifest.xml refactoring some code.

--- lib/Logic/weather.dart
+++ lib/Logic/weather.dart
@@ -1,11 +1,10 @@
-
 import 'package:weather/weather.dart';
+import 'package:http/http.dart' as http;
 
-
-Future<Weather> getWeatherByLongLat(double lat, double lon) async
-{
-  WeatherFactory wf = WeatherFactory("1c1a1b5bc5706b35790855762fe5b8c3", language: Language.GERMAN);
-  Weather weather = await wf.currentWeatherByLocation(lat,lon);
+Future<Weather> getWeatherByLongLat(final double lat, final double lon) async {
+  final WeatherFactory wf = WeatherFactory("1c1a1b5bc5706b35790855762fe5b8c3",
+      language: Language.GERMAN);
+  final Weather weather = await wf.currentWeatherByLocation(lat, lon);
   print(weather.temperature);
   return weather;
 }
