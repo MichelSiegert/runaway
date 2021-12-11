0b6504c67e5ac0bae735cff149ac0d95cbdb9897
Michel Siegert <michel.siegert@student.fh-kiel.de>
2021-12-11 13:41:16 +0100

    making an implementation using a CURL to get data that we want.

--- lib/Logic/weather.dart
+++ lib/Logic/weather.dart
@@ -1,5 +1,7 @@
-import 'package:weather/weather.dart';
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
+import 'package:weather/weather.dart';
 
 Future<Weather> getWeatherByLongLat(final double lat, final double lon) async {
   final WeatherFactory wf = WeatherFactory("1c1a1b5bc5706b35790855762fe5b8c3",
@@ -7,4 +9,13 @@ Future<Weather> getWeatherByLongLat(final double lat, final double lon) async {
   final Weather weather = await wf.currentWeatherByLocation(lat, lon);
   print(weather.temperature);
   return weather;
+}
+
+getWeatherInArea(final double lat, final double lon) async {
+  final url = Uri.parse(
+      "https://api.openweathermap.org/data/2.5/find?lat=$lat&lon=$lon&cnt=50&appid=1c1a1b5bc5706b35790855762fe5b8c3&lang=de&units=metric");
+
+  final result = await http.post(url);
+  final parsed = jsonDecode(result.body);
+  print(parsed);
 }
