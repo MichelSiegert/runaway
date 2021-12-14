15957a9187afc4ddd29a1dfee3114a83497cc05c
MichelSiegert <40004350+MichelSiegert@users.noreply.github.com>
2021-12-14 10:49:16 +0100

    calculating the size of the box relative to the size of the screen

--- lib/Widgets/loading_animation.dart
+++ lib/Widgets/loading_animation.dart
@@ -4,10 +4,11 @@ Future loadingAnimation(BuildContext context) => showDialog(
     context: context,
     builder: (BuildContext context) => Dialog(
         child: SizedBox(
-            //TODO make this not hard coded!
-            height: 100,
+            height: MediaQuery.of(context).orientation == Orientation.portrait
+                ? MediaQuery.of(context).size.height * 0.3
+                : MediaQuery.of(context).size.width * 0.3,
             child: Column(
-              // max = the height of the sizedbox in this case.
+                // max = the height of the sizedbox in this case.
                 mainAxisSize: MainAxisSize.max,
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
