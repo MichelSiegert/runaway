48dded147a7cfd72764fad07cdf3f3b570d9ec5f
MichelSiegert <40004350+MichelSiegert@users.noreply.github.com>
2021-12-16 14:07:03 +0100

    implementing access to the database to check if the information is available or not.

--- lib/Logic/database.dart
+++ lib/Logic/database.dart
@@ -1,11 +1,30 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 
-writeToDatabase() async {
+void tapWeatherCardToDataBase(String place) async {
   final DatabaseReference database = FirebaseDatabase.instance.ref();
   final FirebaseAuth auth = FirebaseAuth.instance;
   final User? us = auth.currentUser;
   final String uid = us!.uid;
-  final DatabaseReference reference = database.child("/users/$uid");
-  reference.set({"does this work": "maybe", "will we see that?": "yes!"});
+  final DatabaseReference reference =
+      //TODO ich weiß nicht wie man solche datenbanken ordentlich modelliert!
+      database.child("/users/$uid/favorites/$place");
+  final DatabaseEvent getData = await reference.once();
+  if (getData.snapshot.value == null) {
+    reference.set({place: "isFavorite"});
+  } else {
+    reference.set(null);
+  }
+}
+
+Future isThisPlaceAFavoriteOfUser(String place) async {
+  final DatabaseReference database = FirebaseDatabase.instance.ref();
+  final FirebaseAuth auth = FirebaseAuth.instance;
+  final User? us = auth.currentUser;
+  final String uid = us!.uid;
+  final DatabaseReference reference =
+      database.child("/users/$uid/favorites/$place");
+  final DatabaseEvent getData = await reference.once();
+  bool result = !(getData.snapshot.value == null);
+  return result;
 }
