dec2f52e0fe650079cb1907a4c6e3ea1e52abeeb
Michel Siegert <michel.siegert@student.fh-kiel.de>
2021-12-19 13:46:38 +0100

    database code.

--- lib/Logic/database.dart
+++ lib/Logic/database.dart
@@ -4,10 +4,10 @@ import 'package:firebase_database/firebase_database.dart';
 void tapWeatherCardToDataBase(String place) async {
   final DatabaseReference database = FirebaseDatabase.instance.ref();
   final FirebaseAuth auth = FirebaseAuth.instance;
-  final User? us = auth.currentUser;
-  final String uid = us!.uid;
+  final User us = auth.currentUser!;
+  final String uid = us.uid;
+  //TODO ich weiß nicht wie man solche datenbanken ordentlich modelliert!
   final DatabaseReference reference =
-      //TODO ich weiß nicht wie man solche datenbanken ordentlich modelliert!
       database.child("/users/$uid/favorites/$place");
   final DatabaseEvent getData = await reference.once();
   if (getData.snapshot.value == null) {
@@ -23,8 +23,41 @@ Future isThisPlaceAFavoriteOfUser(String place) async {
   final User? us = auth.currentUser;
   final String uid = us!.uid;
   final DatabaseReference reference =
-      database.child("/users/$uid/favorites/$place");
+  database.child("/users/$uid/favorites/$place");
   final DatabaseEvent getData = await reference.once();
   bool result = !(getData.snapshot.value == null);
   return result;
+}
+
+Future<void> LanguageToDb(String language) async {
+  final DatabaseReference database = FirebaseDatabase.instance.ref();
+  final FirebaseAuth auth = FirebaseAuth.instance;
+  final User us = auth.currentUser!;
+  final String uid = us.uid;
+
+  final DatabaseReference reference =
+  database.child("/users/$uid/settings/language");
+  reference.set({"lang": language});
+}
+
+Future<void> LanguageDisplayToDb(String display) async {
+  final DatabaseReference database = FirebaseDatabase.instance.ref();
+  final FirebaseAuth auth = FirebaseAuth.instance;
+  final User us = auth.currentUser!;
+  final String uid = us.uid;
+
+  final DatabaseReference reference =
+  database.child("/users/$uid/settings/languageDisplay");
+  reference.set({"display": display});
+}
+
+Future<void> MetricToDb(String metric) async {
+  final DatabaseReference database = FirebaseDatabase.instance.ref();
+  final FirebaseAuth auth = FirebaseAuth.instance;
+  final User us = auth.currentUser!;
+  final String uid = us.uid;
+
+  final DatabaseReference reference =
+  database.child("/users/$uid/settings/metrics");
+  reference.set({"metric": metric});
 }
