1746d55fca1c0679952a8f29a9324011b8014bdf
MichelSiegert <40004350+MichelSiegert@users.noreply.github.com>
2021-12-19 23:18:31 +0100

    adjusting the style of the page

--- lib/Pages/Settings/settings_scaffold.dart
+++ lib/Pages/Settings/settings_scaffold.dart
@@ -5,7 +5,6 @@ import 'settingsDropDownWidgets/language.dart';
 import 'settingsDropDownWidgets/language_display.dart';
 import 'package:flutter/material.dart';
 
-
 //
 Scaffold settingScaffold(final String title, BuildContext context) => Scaffold(
       appBar: AppBar(
@@ -13,28 +12,32 @@ Scaffold settingScaffold(final String title, BuildContext context) => Scaffold(
         backgroundColor: Colors.green,
       ),
       //Text in the middle of the page
-      body: SizedBox.expand(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          Row(
-            mainAxisAlignment:  MainAxisAlignment.spaceAround ,
-            children: [const Text("Language Style"), DropDownLanguage()],
-          ),
-          const Divider(),
-          //Sprache
-          Row(
-            mainAxisAlignment:  MainAxisAlignment.spaceAround ,
-            children: [const Text("Language"), Languages()],
-          ),
-          const Divider(),
-          //Temperatureinheit:
-          Row(
-            mainAxisAlignment:  MainAxisAlignment.spaceAround ,
-            children: [const Text("Unit"), UnitSystems()],
-          ),
-        ],
-      )),
+      body: Container(
+        padding: const EdgeInsets.all(16),
+                child: SizedBox.expand(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [const Text("Language Style"), DropDownLanguage()],
+            ),
+            const Divider(),
+            //Sprache
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [const Text("Language"), Languages()],
+            ),
+            const Divider(),
+            //Temperatureinheit:
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [const Text("Unit"), UnitSystems()],
+            ),
+            const Divider(),
+          ],
+        )),
+      ),
       //Menu Sidebar
       drawer: createSidebar(context),
     );
