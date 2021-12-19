4117f8dc4b5d207b245a26b580ce33033cc0d9f1
MichelSiegert <40004350+MichelSiegert@users.noreply.github.com>
2021-12-19 23:17:28 +0100

    changing the codestyle and adding dependencies.

--- lib/Pages/Settings/settingsDropDownWidgets/language.dart
+++ lib/Pages/Settings/settingsDropDownWidgets/language.dart
@@ -1,3 +1,4 @@
+import 'package:away/Database/settings.dart';
 import 'package:flutter/material.dart';
 
 class Languages extends StatefulWidget {
@@ -13,17 +14,25 @@ class LanguagesState extends State<Languages> {
   Widget build(BuildContext context) {
     return DropdownButton<String>(
       value: widget.dropdownValue,
-      items: <String>["Latinus","Deutsch", "English", "tlhInganpu\'", "Esperanto"]
+      //TODO this solution has to be better.
+      items: <String>[
+        "Latinus",
+        "Deutsch",
+        "English",
+        "tlhInganpu\'",
+        "Esperanto"
+      ]
           .map<DropdownMenuItem<String>>(
               (String entry) => DropdownMenuItem<String>(
-            value: entry,
-            child: Text(entry),
-          ))
+                    value: "en",
+                    child: Text(entry),
+                  ))
           .toList(),
       onChanged: (String? newVal) {
+        //TODO setSettings is for db, rename
+        setSettings("lang", newVal!);
         setState(() {
-          //TODO hier muss noch das in die DB gespeichert werden.
-          widget.dropdownValue = newVal!;
+          widget.dropdownValue = newVal;
         });
       },
     );
