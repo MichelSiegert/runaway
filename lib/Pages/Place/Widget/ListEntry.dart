6418080032545d342295de1e4c5a76c30ed13989
michel.siegert <michel.siegert@student.fh-kiel.de>
2022-01-01 17:24:26 +0100

    refactoring place_scaffold to improve the quality of the project.

---
+++ lib/Pages/Place/Widget/ListEntry.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ListEntry extends StatelessWidget {
+  final String text, value;
+
+  const ListEntry({required this.text, required this.value, Key? key}) :super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [Text(text), Text(value)],
+      );
+}
