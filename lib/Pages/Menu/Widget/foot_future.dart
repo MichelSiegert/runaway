bb0b32e69becb3f543f81ba12ac3090af9a364ba
michel.siegert <michel.siegert@student.fh-kiel.de>
2022-01-05 15:25:52 +0100

    adjust the color of the footer to its real color while loading to give a sense of layout. fixing some codestyle issues.

--- lib/Pages/Menu/Widget/foot_future.dart
+++ lib/Pages/Menu/Widget/foot_future.dart
@@ -1,7 +1,7 @@
 import 'package:away/Logic/Database/settings.dart';
 import 'package:away/Logic/location.dart';
+import 'package:away/Logic/screen_size_calculator.dart';
 import 'package:away/Logic/weather.dart';
-import 'package:away/Widgets/load_scaff.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:weather/weather.dart';
@@ -19,26 +19,29 @@ class BottomNavigationBarFuture extends StatelessWidget {
           if (snapshot.hasData) {
             Weather w = snapshot.data![0] as Weather;
             String unit = snapshot.data![1];
-            bool isCelsius = unit=="metric";
+            bool isCelsius = unit == "metric";
             return MenuFooter(weather: w, isCelsius: isCelsius);
-          }
-          else if (snapshot.hasError) {
+          } else if (snapshot.hasError) {
             return Text(snapshot.error.toString());
-          }
-          else if (!(snapshot.hasError || snapshot.hasData)) {
-            return const LoadingScaffold();
-          }
-          else {
+          } else if (!(snapshot.hasError || snapshot.hasData)) {
+            return SizedBox(
+              width: calculateWidth(1, context),
+              height: calculateWidth(0.15, context),
+              child: Container(
+                color: Colors.blueGrey[900],
+                child: const Center(
+                  child: CircularProgressIndicator(),
+                ),
+              ),
+            );
+          } else {
             return const Text("forgot to implement a case in FOOT_FUTURE");
           }
-        }
-    );
+        });
   }
 
-        Future getWeatherOfLocation()
-    async
-    {
-      Position pos = await getLongLat();
-      return await getWeatherByLatLon(pos.latitude, pos.longitude);
-    }
+  Future getWeatherOfLocation() async {
+    Position pos = await getLongLat();
+    return await getWeatherByLatLon(pos.latitude, pos.longitude);
   }
+}
