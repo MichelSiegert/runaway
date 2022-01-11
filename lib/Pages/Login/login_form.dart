e1bae3b2f19d0c45b58eccd5e8f619e071e7edc1
michel <michel.siegert@student.fh-kiel.de>
2022-01-11 18:56:52 +0100

    improving code struct and making the textfields the same size.

--- lib/Pages/Login/login_form.dart
+++ lib/Pages/Login/login_form.dart
@@ -1,4 +1,5 @@
 import 'package:away/Logic/authentication.dart';
+import 'package:away/Logic/screen_size_calculator.dart';
 import 'package:away/Widgets/loading_animation.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -14,75 +15,68 @@ class LoginForm extends StatelessWidget {
     TextEditingController passCon = TextEditingController();
     final AuthService _authent = AuthService();
     final List<Widget> elements = [];
-    elements.add(SizedBox(height: 50,));
-    elements.add(TextField(
-    controller: textCon,
-      obscureText: false,
-      decoration: const InputDecoration(
-          border: OutlineInputBorder(), labelText: "Email"),
-    ));
-    elements.add(Container(
-      padding: const EdgeInsets.all(10),
-      child: TextField(
+
+    elements.add(SizedBox(
+        width: calculateWidth(0.6, context),
+        height: calculateHeight(0.1, context),
+        child: TextField(
+          controller: textCon,
+          obscureText: false,
+          decoration: const InputDecoration(
+              border: OutlineInputBorder(), labelText: "Email"),
+        )));
+
+    elements.add(
+      TextField(
         obscureText: true,
         decoration: const InputDecoration(
             border: OutlineInputBorder(), labelText: "Passwort"),
-          controller: passCon,
+        controller: passCon,
       ),
-    ));
+    );
 
     elements.add(Column(children: <Widget>[
-      Container(
-        padding: const EdgeInsets.all(10),
-        child: ElevatedButton(
-          style: ElevatedButton.styleFrom(primary: Colors.blueGrey[900]),
-          child: const Text("login"),
-          onPressed: () async {
-            showDialog(
-                context: context,
-                builder: (BuildContext context) => LoadingDialog());
+      ElevatedButton(
+        style: ElevatedButton.styleFrom(primary: Colors.blueGrey[900]),
+        child: const Text("login"),
+        onPressed: () async {
+          showDialog(
+              context: context,
+              builder: (BuildContext context) => LoadingDialog());
 
-            final user = await _authent.login(textCon.text, passCon.text);
-            Navigator.pop(context); //pop dialog
-            if (user.runtimeType != UserCredential) {
-              showAlertDialog(context, user);
-            } else {
-              Navigator.pushNamed(context, "/menu");
-            }
-          },
-        ),
+          final user = await _authent.login(textCon.text, passCon.text);
+          Navigator.pop(context); //pop dialog
+          if (user.runtimeType != UserCredential) {
+            showAlertDialog(context, user);
+          } else {
+            Navigator.pushNamed(context, "/menu");
+          }
+        },
       ),
-      Container(
-        padding: const EdgeInsets.all(10),
-        child: ElevatedButton(
+      ElevatedButton(
+        style: ElevatedButton.styleFrom(primary: Colors.blueGrey[900]),
+        child: const Text("login anonymously"),
+        onPressed: () async {
+          showDialog(
+              context: context,
+              builder: (BuildContext context) => LoadingDialog());
+          User? user = await _authent.signInAnon();
+          Navigator.pop(context);
+          if (user == null) {
+            showAlertDialog(context, "Failed to sign in anon!");
+          } else {
+            Navigator.pushNamed(context, "/menu");
+          }
+        },
+      ),
+      ElevatedButton(
+          child: const Text("No Account? Register here!"),
           style: ElevatedButton.styleFrom(primary: Colors.blueGrey[900]),
-          child: const Text("login anonymously"),
-          onPressed: () async {
-            showDialog(
-                context: context,
-                builder: (BuildContext context) => LoadingDialog());
-            User? user = await _authent.signInAnon();
-            Navigator.pop(context);
-            if (user == null) {
-              showAlertDialog(context, "Failed to sign in anon!");
-            } else {
-              Navigator.pushNamed(context, "/menu");
-            }
-          },
-        ),
-      ),
-      Container(
-        padding: const EdgeInsets.all(10),
-        child: ElevatedButton(
-            child: const Text("No Account? Register here!"),
-            style: ElevatedButton.styleFrom(primary: Colors.blueGrey[900]),
-            onPressed: () {
-              Navigator.pushNamed(context, "/register");
-            }),
-      ),
+          onPressed: () {
+            Navigator.pushNamed(context, "/register");
+          }),
       SignInButton(
         Buttons.GoogleDark,
-        padding: const EdgeInsets.all(10),
         onPressed: () async {
           _authent.signInWithGoogle().then((user) {
             if (user == null) {
@@ -101,8 +95,10 @@ class LoginForm extends StatelessWidget {
 
     return SizedBox.expand(
       child: Center(
-        child: ListView(
-            children: elements),
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          child: ListView(children: elements),
+        ),
       ),
     );
   }
@@ -116,8 +112,7 @@ showAlertDialog(BuildContext context, String text) {
     },
   );
   AlertDialog alert = AlertDialog(
-    title: Text(text,
-        style: TextStyle(color: Colors.red)),
+    title: Text(text, style: TextStyle(color: Colors.red)),
     actions: [
       okButton,
     ],
